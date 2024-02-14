b2a7037d665617dadc7d983b306ea540f768c323
Denis Lukianenko <denlyk1@gmail.com>
2024-02-14 19:55:20 +0200

    feat: add Box and Spacer components to design system

---
+++ src/lib/design-system/components/Box/Box.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	type Props = {
+		children: Snippet;
+	};
+
+	let { children } = $props<Props>();
+</script>
+
+<span class="box">
+	{@render children()}
+</span>
+
+<style>
+	.box {
+		display: flex;
+		flex-direction: column;
+		background: var(--color-box-bg);
+		border-radius: var(--size-box-border-radius);
+		box-shadow: var(--shadow-box);
+		padding-top: var(--size-box-padding-top);
+		padding-bottom: var(--size-box-padding-bottom);
+		padding-inline: var(--size-box-padding-inline);
+	}
+</style>
