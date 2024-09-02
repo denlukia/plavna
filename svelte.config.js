a3ec1dfe5365d5780857a41186871d4ed3bb2d28
Denis Lukianenko <denlyk1@gmail.com>
2024-09-02 10:14:51 +0300

    fix: remove splitting of fns to maybe fix routing

--- svelte.config.js
+++ svelte.config.js
@@ -15,8 +15,8 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			runtime: 'edge',
-			regions: ['fra1'],
-			split: true
+			regions: ['fra1']
+			// split: true
 		})
 	}
 };
