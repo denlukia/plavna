95d08822d4c92ed6fef9a28684cbfae5b900cca7
Denis Lukianenko <denlyk1@gmail.com>
2024-09-02 10:43:29 +0300

    fix: add fix-roroute-split version of adapter-vercel to maybe fix reroutes

---
+++ src/lib/adapter-vercel/ambient.d.ts
@@ -0,0 +1,14 @@
+import type { RequestContext } from './index.js';
+
+declare global {
+	namespace App {
+		export interface Platform {
+			/**
+			 * `context` is only available in Edge Functions
+			 */
+			context?: RequestContext;
+		}
+	}
+}
+
+export {};
