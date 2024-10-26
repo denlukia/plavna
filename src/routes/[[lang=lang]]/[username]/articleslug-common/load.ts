d98a7fdc75ac948e8cbb4a9b6204e514f5fb6e08
Denis Lukianenko <denlyk1@gmail.com>
2024-10-26 03:12:14 +0300

    fix: article translations now correctly are transformed to state
    also fix: remove edit button on article page for non author

--- src/routes/[[lang=lang]]/[username]/articleslug-common/load.ts
+++ src/routes/[[lang=lang]]/[username]/articleslug-common/load.ts
@@ -1,11 +1,15 @@
+import { createRecordsTranslationsState } from '$lib/features/i18n/state.svelte';
+
 import type { PageLoad } from '../[articleslug]/$types';
 
 export const load = (async ({ data }) => {
-	data = structuredClone(data);
+	const { recordsTranslations, ...other } = structuredClone(data);
 	// let previewType = data.previewType;
 	// if (previewType !== null) {
 	// 	previewType = previewType as PreviewTypeSelect;
 	// 	previewType.component_editor = await getPreviewComponent(previewType.url, 'viewer');
 	// }
-	return { ...data };
+	const recordsTranslationsState = createRecordsTranslationsState(recordsTranslations);
+
+	return { ...other, recordsTranslationsState };
 }) satisfies PageLoad;
