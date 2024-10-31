c88f5823556ead1e05c6a957c50dca4a40d89ef2
Denis Lukianenko <denlyk1@gmail.com>
2024-10-31 03:16:14 +0200

    style: improve timings and use blurred fly animation

--- src/lib/features/animations/PageAnimator.svelte
+++ src/lib/features/animations/PageAnimator.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import type { Snippet } from 'svelte';
 	import { cubicOut } from 'svelte/easing';
-	import { fly } from 'svelte/transition';
+	import { fly } from '$lib/design/transitions/fly';
 
 	type Props = Record<string, any> & {
 		children: Snippet;
@@ -13,7 +13,7 @@
 {#key Object.values(keys || {}).join()}
 	<div
 		class="animation-wrapper"
-		in:fly|global={{ duration: 400, delay: 200, y: 10, easing: cubicOut }}
+		in:fly|global={{ duration: 400, delay: 150, y: 10, easing: cubicOut }}
 		out:fly|global={{ duration: 400, y: -10, easing: cubicOut }}
 	>
 		{@render children()}
