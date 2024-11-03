3269d4555853fe29cd5dfed86a6b3c0e332b7b61
Denis Lukianenko <denlyk1@gmail.com>
2024-11-03 08:37:07 +0200

    style: tweak timings and easing for article cards FLIP animation

--- src/lib/features/article/ArticlesList.svelte
+++ src/lib/features/article/ArticlesList.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { flip } from 'svelte/animate';
-	import { cubicInOut } from 'svelte/easing';
+	import { expoInOut } from 'svelte/easing';
 	import { fade } from 'svelte/transition';
 	import GridContainer from '$lib/design/components/Grid/GridContainer.svelte';
 
@@ -18,9 +18,9 @@
 	{#each section.articles as article (article.meta.id)}
 		<div
 			class="animate-wrapper"
-			animate:flip={{ duration: 300, easing: cubicInOut }}
-			in:fade={{ duration: 200, delay: 100 }}
-			out:fade={{ duration: 200 }}
+			animate:flip={{ duration: 800, easing: expoInOut }}
+			in:fade={{ duration: 500, delay: 300 }}
+			out:fade={{ duration: 500 }}
 		>
 			<ArticlePreviewCell {article} />
 		</div>
