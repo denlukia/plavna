5abdfff7ef2c29756d6f55c37c878d5e05676b05
Denis Lukianenko <denlyk1@gmail.com>
2024-12-17 19:52:57 +0200

    style: improve mobile typograhy and add font preloads (#266)

---
+++ src/lib/design/components/ThemeInjector/FontPreloadToHead.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	type Props = {
+		links: string[];
+	};
+
+	let { links }: Props = $props();
+</script>
+
+<svelte:head>
+	{#each links as l}
+		{@const type = l.split('.').pop()}
+		<link rel="preload" href={l} as="font" type="font/{type}" crossorigin="anonymous" />
+	{/each}
+</svelte:head>
--- src/lib/design/themes/typography/aesthetic/sequences/Index.svelte
+++ src/lib/design/themes/typography/aesthetic/sequences/Index.svelte
@@ -1,9 +1,18 @@
 <script>
 	import StyleToHead from '$lib/design/components/ThemeInjector/CSSToHead.svelte';
+	import FontPreloadToHead from '$lib/design/components/ThemeInjector/FontPreloadToHead.svelte';
 
+	import InterItalicWoff2 from '../../fonts/Inter/InterVariable-Italic.woff2';
+	import InterWoff2 from '../../fonts/Inter/InterVariable.woff2';
+	import SourceSerifItalicWoff2 from '../../fonts/Source Serif 4/SourceSerif4Variable-Italic.otf.woff2';
+	import SourceSerifRomanWoff2 from '../../fonts/Source Serif 4/SourceSerif4Variable-Roman.otf.woff2';
 	import Fonts from './fonts.css?inline';
 	import Typography from './typography.css?inline';
 </script>
+
+<FontPreloadToHead
+	links={[InterItalicWoff2, InterWoff2, SourceSerifItalicWoff2, SourceSerifRomanWoff2]}
+/>
 
 <StyleToHead css={Fonts} />
 <StyleToHead css={Typography} />
