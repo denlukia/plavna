1fcd6b3c4fb91a717a9e3d260d6710ad8eb45096
Denis Lukianenko <denlyk1@gmail.com>
2025-03-18 03:39:28 +0200

    fix: remove server image color probing (breaks in vercel edge)

--- packages/image-uploader/src/lib/images/class/server.ts
+++ packages/image-uploader/src/lib/images/class/server.ts
@@ -1,6 +1,7 @@
-import getPixels from 'get-pixels';
+// import getPixels from 'get-pixels';
 
-import type { NdArray, SizeAndType } from '../types';
+// import type { NdArray, SizeAndType } from '../types';
+import type { SizeAndType } from '../types';
 import { ImageHandler } from './base';
 
 // imagekit-javascript in base class uses XHR for upload
@@ -11,39 +12,41 @@ import '../xhr-polyfill';
 // so we can look at executing color probe separately in Serverless function
 // and use Sharp for that (to support more formats)
 export class ServerImageHandler extends ImageHandler {
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	protected async prepareForColorProbe(fileOrBuffer: File | Buffer, sizeAndType: SizeAndType) {
-		type Pixels = NdArray<Uint8Array>;
+		// type Pixels = NdArray<Uint8Array>;
 
-		const pixelsPromise = new Promise<Pixels>((resolve, reject) => {
-			type Callback = (err: Error | null, pixels: Pixels) => void;
-			const callback: Callback = (err, pixels) => {
-				if (err) {
-					reject(err);
-				}
-				resolve(pixels);
-			};
+		// const pixelsPromise = new Promise<Pixels>((resolve, reject) => {
+		// 	type Callback = (err: Error | null, pixels: Pixels) => void;
+		// 	const callback: Callback = (err, pixels) => {
+		// 		if (err) {
+		// 			reject(err);
+		// 		}
+		// 		resolve(pixels);
+		// 	};
 
-			if (fileOrBuffer instanceof File) {
-				fileOrBuffer.arrayBuffer().then((arrayBuffer) => {
-					const buffer = Buffer.from(arrayBuffer);
-					getPixels(buffer, sizeAndType.mime, callback);
-				});
-			} else {
-				getPixels(fileOrBuffer, sizeAndType.mime, callback);
-			}
-		});
+		// 	if (fileOrBuffer instanceof File) {
+		// 		fileOrBuffer.arrayBuffer().then((arrayBuffer) => {
+		// 			const buffer = Buffer.from(arrayBuffer);
+		// 			getPixels(buffer, sizeAndType.mime, callback);
+		// 		});
+		// 	} else {
+		// 		getPixels(fileOrBuffer, sizeAndType.mime, callback);
+		// 	}
+		// });
 
-		try {
-			const pixels = await pixelsPromise;
-			const { width, height } = sizeAndType;
-			return {
-				width,
-				height,
-				data: Array.from(pixels.data)
-			};
-		} catch (err) {
-			console.error(err);
-			return null;
-		}
+		// try {
+		// 	const pixels = await pixelsPromise;
+		// 	const { width, height } = sizeAndType;
+		// 	return {
+		// 		width,
+		// 		height,
+		// 		data: Array.from(pixels.data)
+		// 	};
+		// } catch (err) {
+		// 	console.error(err);
+		// 	return null;
+		// }
+		return null;
 	}
 }
