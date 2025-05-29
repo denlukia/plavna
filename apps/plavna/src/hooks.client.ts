c0f7915442f1a72fe0abd291fbf42aeec01f86c5
Denis Lukianenko <denlyk1@gmail.com>
2025-05-29 21:43:28 +0300

    Add Sentry via @jill64/sentry-sveltekit-edge

--- apps/plavna/src/hooks.client.ts
+++ apps/plavna/src/hooks.client.ts
@@ -1,22 +1,9 @@
-import { handleErrorWithSentry, replayIntegration } from "@sentry/sveltekit";
-import * as Sentry from '@sentry/sveltekit';
+// hooks.client.js
+import { init } from '@jill64/sentry-sveltekit-edge/client';
 
-Sentry.init({
-  dsn: 'https://378a9658ae1cc68c845e4b8be36dfee0@o4509407519965184.ingest.de.sentry.io/4509407521603664',
+const onError = init(
+	'https://378a9658ae1cc68c845e4b8be36dfee0@o4509407519965184.ingest.de.sentry.io/4509407521603664',
+	{ sentryOptions: { tracesSampleRate: 1 } }
+);
 
-  tracesSampleRate: 1.0,
-
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
-
-  // If the entire session is not sampled, use the below sample rate to sample
-  // sessions when an error occurs.
-  replaysOnErrorSampleRate: 1.0,
-
-  // If you don't want to use Session Replay, just remove the line below:
-  integrations: [replayIntegration()],
-});
-
-// If you have a custom error handler, pass it to `handleErrorWithSentry`
-export const handleError = handleErrorWithSentry();
+export const handleError = onError();
