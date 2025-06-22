d875dff04e827c573b96a7c9f39ebe4aa1e38e53
Denis Lukianenko <denlyk1@gmail.com>
2025-06-22 03:52:38 +0300

    Multi-task screenshotter

--- apps/screenshotter/src/lib/page.ts
+++ apps/screenshotter/src/lib/page.ts
@@ -1,12 +1,18 @@
+import { env } from '$env/dynamic/private';
 import type { Browser, Page } from 'puppeteer-core';
 
 import { wait } from './utils';
 
-export async function getPage(browser: Browser) {
+export async function createPage(browser: Browser) {
 	return browser.newPage();
 }
 
-export async function loadPage(page: Page, url: string, width: number, height: number) {
+export async function loadUrlOnPage(page: Page, url: string, width: number, height: number) {
+	// Replace preview URL if configured
+	if (env.REPLACE_PREVIEW_URL_IN && env.REPLACE_PREVIEW_URL_OUT) {
+		url = url.replace(env.REPLACE_PREVIEW_URL_IN, env.REPLACE_PREVIEW_URL_OUT);
+	}
+
 	await page.setViewport({
 		width: width || 640,
 		height: height || 480,
