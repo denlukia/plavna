1f8418be11ccd5e38be059252e8cd08ae7f4a445
Denis Lukianenko <denlyk1@gmail.com>
2025-06-23 05:23:24 +0300

    Fix applecore theme

---
+++ apps/plavna/src/lib/styles/themes/color/applecore/Index.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { ThemeStyleToHead } from '@plavna/design/theming/components';
+
+	import Colors from './colors.css?inline';
+</script>
+
+<ThemeStyleToHead css={Colors} type="color" id="applecore" />
